29f8494e905ef0b6a55600e64283a0daaf6fa4b7
Vishal Pansare <51355863+vishalpansare5600@users.noreply.github.com>
2023-03-15 10:11:38 +0530

    Add files via upload

---
+++ conditionaloperator/src/conditionaloperator/conditionaloperator.java
@@ -0,0 +1,17 @@
+package conditionaloperator;
+
+public class conditionaloperator {
+	
+	public static void main(String args[])
+	
+	{
+		int a=10,b=20,small;
+		if(a<b)
+			small=a;
+		else
+			small=b;
+		System.out.println("small no="+small);
+		
+	}
+
+}
---
+++ conditionaloperator1/src/conditionaloperator1/conditionaloperator1.java
@@ -0,0 +1,14 @@
+package conditionaloperator1;
+
+public class conditionaloperator1 {
+
+	public static void main(String[] args) 
+	{
+	
+		int a=50,b=20,small;
+		small=(a<b)?a:b;
+		System.out.println("small no ="+small);
+
+	}
+
+}
